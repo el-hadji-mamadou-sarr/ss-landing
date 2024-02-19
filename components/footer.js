82116a18e5cc1af696494fd6c3e45672ccd23f94
el-hadji-mamadou-sarr <elhadji.sarr@outlook.com>
2024-02-19 18:30:37 +0100

    fix links

--- components/footer.js
+++ components/footer.js
@@ -5,94 +5,82 @@ import Container from "./container";
 
 export default function Footer() {
   const navigation = ["Product", "Blog", "Support", "Contact"];
- const legal = ["Terms", "Privacy", "Legal"];
- return (
-   <div className="relative">
-     <Container>
-       <div className="grid max-w-screen-xl grid-cols-1 gap-10 pt-10 mx-auto mt-5 border-t border-gray-100 dark:border-trueGray-700 lg:grid-cols-5">
-         <div className="lg:col-span-2">
-           <div>
-             {" "}
-             <Link
-               href="/"
-               className="flex items-center space-x-2 text-2xl font-medium text-indigo-500 dark:text-gray-100"
-             >
-               <Image src="/img/logo.svg" alt="SS" width="150" height="50" />
-             </Link>
-           </div>
-         </div>
+  const legal = ["Terms", "Privacy", "Legal"];
+  return (
+    <div className="relative">
+      <Container>
+        <div className="grid max-w-screen-xl grid-cols-1 gap-10 pt-10 mx-auto mt-5 border-t border-gray-100 dark:border-trueGray-700 lg:grid-cols-5">
+          <div className="lg:col-span-2">
+            <div>
+              {" "}
+              <Link
+                href="/"
+                className="flex items-center space-x-2 text-2xl font-medium text-indigo-500 dark:text-gray-100"
+              >
+                <Image src="/img/logo.svg" alt="SS" width="150" height="50" />
+              </Link>
+            </div>
+          </div>
 
-         <div>
-           <div className="flex flex-wrap w-full -mt-2 -ml-3 lg:ml-0">
-             {navigation.map((item, index) => (
-               <Link
-                 key={index}
-                 href="/"
-                 className="w-full px-4 py-2 text-gray-500 rounded-md dark:text-gray-300 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:focus:bg-trueGray-700"
-               >
-                 {item}
-               </Link>
-             ))}
-           </div>
-         </div>
-         <div>
-           <div className="flex flex-wrap w-full -mt-2 -ml-3 lg:ml-0">
-             {legal.map((item, index) => (
-               <Link
-                 key={index}
-                 href="/"
-                 className="w-full px-4 py-2 text-gray-500 rounded-md dark:text-gray-300 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:focus:bg-trueGray-700"
-               >
-                 {item}
-               </Link>
-             ))}
-           </div>
-         </div>
-         <div className="">
-           <div>Follow us</div>
-           <div className="flex mt-5 space-x-5 text-gray-400 dark:text-gray-500">
-             <a
-               href="https://twitter.com/web3templates"
-               target="_blank"
-               rel="noopener"
-             >
-               <span className="sr-only">Twitter</span>
-               <Twitter />
-             </a>
-             <a
-               href="https://twitter.com/web3templates"
-               target="_blank"
-               rel="noopener"
-             >
-               <span className="sr-only">Discord</span>
-               <Discord />
-             </a>
-             <a
-               href="https://facebook.com/web3templates"
-               target="_blank"
-               rel="noopener"
-             >
-               <span className="sr-only">Facebook</span>
-               <Facebook />
-             </a>
-             <a
-               href="https://instagram.com/web3templates"
-               target="_blank"
-               rel="noopener"
-             >
-               <span className="sr-only">Instagram</span>
-               <Instagram />
-             </a>
-             <a href="https://linkedin.com/" target="_blank" rel="noopener">
-               <span className="sr-only">Linkedin</span>
-               <Linkedin />
-             </a>
-           </div>
-         </div>
-       </div>
-     </Container>
-   </div>
- );
+          <div>
+            <div className="flex flex-wrap w-full -mt-2 -ml-3 lg:ml-0">
+              {navigation.map((item, index) => (
+                <Link
+                  key={index}
+                  href="/"
+                  className="w-full px-4 py-2 text-gray-500 rounded-md dark:text-gray-300 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:focus:bg-trueGray-700"
+                >
+                  {item}
+                </Link>
+              ))}
+            </div>
+          </div>
+          <div>
+            <div className="flex flex-wrap w-full -mt-2 -ml-3 lg:ml-0">
+              {legal.map((item, index) => (
+                <Link
+                  key={index}
+                  href="/"
+                  className="w-full px-4 py-2 text-gray-500 rounded-md dark:text-gray-300 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:focus:bg-trueGray-700"
+                >
+                  {item}
+                </Link>
+              ))}
+            </div>
+          </div>
+          <div className="">
+            <div>Follow us</div>
+            <div className="flex mt-5 space-x-5 text-gray-400 dark:text-gray-500">
+              <a
+                href="https://discord.gg/CskfJRhG"
+                target="_blank"
+                rel="noopener"
+              >
+                <span className="sr-only">Discord</span>
+                <Discord />
+              </a>
+              <a href="#" target="_blank" rel="noopener">
+                <span className="sr-only">Twitter</span>
+                <Twitter />
+              </a>
+              <a href="#" target="_blank" rel="noopener">
+                <span className="sr-only">Facebook</span>
+                <Facebook />
+              </a>
+              <a href="#" target="_blank" rel="noopener">
+                <span className="sr-only">Instagram</span>
+                <Instagram />
+              </a>
+              <a href="#" target="_blank" rel="noopener">
+                <span className="sr-only">Linkedin</span>
+                <Linkedin />
+              </a>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
 }
 
 const Twitter = ({ size = 24 }) => (
@@ -109,12 +97,12 @@ const Twitter = ({ size = 24 }) => (
 const Discord = ({ size = 24 }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 127.14 96.36"
     width={size}
     height={size}
-    viewBox="0 0 24 24"
     fill="currentColor"
   >
-    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10c5.52 0 10-4.48 10-10S17.52 2 12 2zm0 18.13c-4.4 0-7.99-3.58-7.99-7.99 0-1.78.58-3.42 1.56-4.75-.03.09-.05.17-.05.26 0 .38.31.69.69.69s.69-.31.69-.69c0-.38-.31-.69-.69-.69-.41 0-.74-.33-.74-.75 0-.18.06-.34.15-.48.04-.08.09-.16.15-.23-2.51.83-4.3 3.25-4.3 6.06 0 3.36 2.72 6.07 6.07 6.07 3.36 0 6.07-2.71 6.07-6.07 0-.34-.03-.67-.08-1 .05-.06.09-.12.14-.19.05-.06.11-.11.17-.17.07-.07.13-.13.2-.19.01-.01.02-.03.03-.04.03-.05.07-.09.1-.14.04-.05.07-.1.1-.15.05-.08.1-.16.14-.24.03-.07.05-.14.07-.22.01-.05.02-.1.03-.16.02-.09.04-.17.05-.26 0-.38-.31-.69-.69-.69-.38 0-.69.31-.69.69 0 .09.02.17.05.25-.3.51-.48 1.09-.48 1.72 0 1.65 1.34 2.99 2.99 2.99s2.99-1.34 2.99-2.99c0-.6-.18-1.16-.48-1.63.3-.08.58-.21.83-.39a3 3 0 0 0-2.17-.97c.32-.33.54-.76.6-1.23 0 0 .01-.05.01-.07 0-.09-.01-.17-.03-.26.33-.03.64-.1.92-.21-.21.28-.45.54-.71.76.03-.05.06-.09.09-.14.09-.12.17-.25.24-.38.02-.05.05-.09.07-.13.04-.09.08-.18.11-.28.04-.12.08-.24.12-.36.04-.14.07-.28.1-.42.03-.14.05-.28.07-.42.02-.16.04-.32.06-.48.01-.09.03-.18.04-.27.01-.1.03-.19.04-.29 0-.01.01-.03.01-.04.01-.05.02-.09.03-.14 0-.04.01-.09.01-.14 0-.06.01-.12.01-.18v-.05c0-.01 0-.01 0-.01 0-.01 0-.01 0-.01l.01-.01h.01c.02-.08.03-.17.05-.25l.01-.04c0-.01 0-.02 0-.02 0-.02.01-.04.01-.06 0-.02.01-.03.01-.05 0-.03.01-.05.01-.08 0-.04.01-.08.01-.12s.01-.07.01-.1c0-.01.01-.02.01-.03 0-.03.01-.06.01-.1 0-.02 0-.03 0-.05 0-.03.01-.05.01-.08 0-.04.01-.09.01-.13 0-.02.01-.04.01-.06 0-.03.01-.06.01-.09 0-.04.01-.08.01-.12s.01-.07.01-.11c0-.03.01-.06.01-.09 0-.03.01-.06.01-.09s.01-.06.01-.09c0-.03.01-.07.01-.1 0-.03.01-.06.01-.09s.01-.06.01-.09c0-.03.01-.07.01-.11c0-.02.01-.04.01-.07 0-.02.01-.03.01-.05 0-.03.01-.06.01-.09 0-.03 0-.06.01-.09 0-.02 0-.04.01-.06s0-.05.01-.08c0-.02.01-.04.01-.06 0-.02.01-.04.01-.06 0-.02.01-.03.01-.05 0-.02.01-.05.01-.07s.01-.04.01-.06c0-.02.01-.03.01-.05s.01-.04.01-.06c0-.02.01-.03.01-.05s.01-.03.01-.04l.01-.02.01-.02c0-.01 0-.01.0 1.44c0 1.44-1.18 2.62-2.62 2.62s-2.62-1.18-2.62-2.62c0-1.08.64-2.01 1.55-2.43-.02.2-.03.41-.03.61 0 2.19 1.73 3.97 3.87 4.1-.4.09-.81.13-1.24.13-.3 0-.59-.03-.88-.08.59 1.82 2.3 3.14 4.32 3.18-1.58 1.24-3.58 1.98-5.75 1.98-.37 0-.74-.02-1.1-.07 2.04 1.3 4.46 2.06 7.06 2.06 8.46 0 13.07-7 13.07-13 .01-.2 0-.4-.02-.59.9-.65 1.68-1.47 2.3-2.4z" />
+    <path d="M107.7,8.07A105.15,105.15,0,0,0,81.47,0a72.06,72.06,0,0,0-3.36,6.83A97.68,97.68,0,0,0,49,6.83,72.37,72.37,0,0,0,45.64,0,105.89,105.89,0,0,0,19.39,8.09C2.79,32.65-1.71,56.6.54,80.21h0A105.73,105.73,0,0,0,32.71,96.36,77.7,77.7,0,0,0,39.6,85.25a68.42,68.42,0,0,1-10.85-5.18c.91-.66,1.8-1.34,2.66-2a75.57,75.57,0,0,0,64.32,0c.87.71,1.76,1.39,2.66,2a68.68,68.68,0,0,1-10.87,5.19,77,77,0,0,0,6.89,11.1A105.25,105.25,0,0,0,126.6,80.22h0C129.24,52.84,122.09,29.11,107.7,8.07ZM42.45,65.69C36.18,65.69,31,60,31,53s5-12.74,11.43-12.74S54,46,53.89,53,48.84,65.69,42.45,65.69Zm42.24,0C78.41,65.69,73.25,60,73.25,53s5-12.74,11.44-12.74S96.23,46,96.12,53,91.08,65.69,84.69,65.69Z" />
   </svg>
 );
 
